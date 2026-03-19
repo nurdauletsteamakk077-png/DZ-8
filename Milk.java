eb60017fd57979d432149993b6ac9c61351c3387
nurdauletsteamakk077-png <nurdauletsteamakk077@gmail.com>
2026-03-19 23:28:37 +0500

    Create Milk.java

---
+++ Milk.java
@@ -0,0 +1,14 @@
+class Milk extends BeverageDecorator {
+
+    public Milk(Beverage beverage) {
+        super(beverage);
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Milk";
+    }
+
+    public double cost() {
+        return beverage.cost() + 0.5;
+    }
+}
