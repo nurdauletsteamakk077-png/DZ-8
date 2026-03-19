b5771bab8f95d894e7dc6264e724b45b199204fb
nurdauletsteamakk077-png <nurdauletsteamakk077@gmail.com>
2026-03-19 23:28:02 +0500

    Create BeverageDecorator.java

---
+++ BeverageDecorator.java
@@ -0,0 +1,7 @@
+abstract class BeverageDecorator implements Beverage {
+    protected Beverage beverage;
+
+    public BeverageDecorator(Beverage beverage) {
+        this.beverage = beverage;
+    }
+}
